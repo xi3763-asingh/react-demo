9bac247b1b57d8b346ec4c21fb86da783f48a114
Avinash Singh <avinash.singh3@indusind.com>
2025-08-05 14:44:29 +0530

    demo app - initial comit

---
+++ src/components/Joke.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import './Joke.css';
+import Button from "./Button";
+
+const Joke = () => {
+    const [Joke, setJoke] = useState("");
+
+    const fetchApi = () => {
+        fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+            .then((res) => res.json())
+            .then((data) => setJoke(data.joke));
+    };
+
+    return (
+        <div className="joke">
+            <Button callApi={fetchApi} /> 
+            <p>{Joke}</p>    
+        </div>
+    );
+}
+
+export default Joke;
